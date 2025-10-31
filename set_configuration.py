eed0ba58cc5c8674a666b642a11bc8473db90469
BulkTornado <ranjanjaiditya@gmail.com>
2025-10-31 22:13:55 +0530

    auto_set_config done

--- set_configuration.py
+++ set_configuration.py
@@ -1,11 +1,13 @@
 import json
 
-with open('proj_settings.json', 'r') as f:
+with open('configuration.json', 'r') as f:
     curr_settings: dict = json.load(f)
 
+user = input('Enter your username, if you set any, during MySQL installation(press Enter without writing anything, if you hadn\'t set anything): ') or 'root'
 passwd = input('Enter the password you had set during MySQL installation: ')
 
+curr_settings['user'] = user
 curr_settings['passwd'] = passwd
 
-with open('proj_settings.json', 'w') as f:
+with open('configuration.json', 'w') as f:
     json.dump(curr_settings, f, indent=4)
